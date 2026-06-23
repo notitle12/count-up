25be99329b8744410042158fb4d23e1854c7f977
HUISU <133619212+notitle12@users.noreply.github.com>
2026-06-23 14:49:15 +0900

    chore: 배포를 위한 base 경로 설정 완료

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 🚨 중요: 깃허브 레포지토리 이름을 앞뒤 슬래시(/)와 함께 꼭 적어주세요!
+  base: '/count-up/', 
 })
